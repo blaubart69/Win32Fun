f4f153e92752a665a189b935627ca72c8feec23c
bee <bernhard.spindler.75@gmail.com>
2018-04-08 18:45:47 +0200

    handling FormatMessage() error

--- beeLib.c
+++ beeLib.c
@@ -13,22 +13,26 @@ void OutWriteLastError(const WCHAR* Apiname)
 	const DWORD LastErr = GetLastError();
 
 	LPWSTR lpWindowsErrorText = L"n/a";
-	if ( FormatMessageW(
-		    FORMAT_MESSAGE_ALLOCATE_BUFFER
-		  | FORMAT_MESSAGE_FROM_SYSTEM
+	DWORD rcFormatMsg;
+	if ((rcFormatMsg = FormatMessageW(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER
+		| FORMAT_MESSAGE_FROM_SYSTEM
 		, NULL
 		, LastErr
 		, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT)
 		, (LPWSTR)&lpWindowsErrorText
 		, 0
-		, NULL) == 0) 
+		, NULL)) == 0)
 	{
-		OutWriteFW(L"E-Win32API: Lasterror: %x, Api: %ls\n", GetLastError(), L"FormatMessage");
+		OutWriteFW(L"E-Win32API: Lasterror: 0x%x, Api: %ls\n", GetLastError(), L"FormatMessage");
+		lpWindowsErrorText = L"!!! no error message available since FormatMessage failed !!!";
 	}
-
 	OutWriteFW(L"E-Win32API: Lasterror: 0x%X, Api: %ls, Msg: %ls\n", LastErr, Apiname, lpWindowsErrorText);
 
-	LocalFree(lpWindowsErrorText);
+	if (rcFormatMsg == 0)
+	{
+		LocalFree(lpWindowsErrorText);
+	}
 }
 
 void OutWriteFW(const WCHAR* format, ...)
